ab9f5bbc9bebdd9fa8ee878163c040a0661f477d
Pierre Gergondet <pierre.gergondet@gmail.com>
2024-02-15 10:35:10 +0900

    Initial commit

---
+++ cmake/mc_int_obj_description_INSTALL_PREFIX.cmake
@@ -0,0 +1,2 @@
+set(mc_int_obj_description_INSTALL_PREFIX "${mc_int_obj_description_DIR}/../")
+get_filename_component(mc_int_obj_description_INSTALL_PREFIX "${mc_int_obj_description_INSTALL_PREFIX}" REALPATH)
